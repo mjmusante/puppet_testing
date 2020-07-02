810e9861c2f654ff5dc90dbe42f1ada233cf1f41
Mark Musante <mark.musante@gmail.com>
2020-07-02 17:49:21 -0400

    ordering

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
     file { '/opt/minecraft/minecraft_server.jar':
         ensure => file,
         source => 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
+        before => Service['minecraft'],
     }
     package { 'java':
         ensure => present,
@@ -20,5 +21,6 @@ class minecraft {
     service { 'minecraft':
         ensure => running,
         enable => true,
+        require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']]
     }
 }
